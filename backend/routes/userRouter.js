60838179f1068a2972bb1660ae62c6b4fefdb20a
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-08-27 16:28:06 +0530

    Working on the login using google

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersController = require("../controllers/usersController.js");
 
 router.post("/users/signUp", usersController.signUp);
+router.post("/users/signIn", usersController.signIn);
 
 module.exports = router;
